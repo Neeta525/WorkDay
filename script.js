570f9de66b566112edd6783aff0b59d87318a134
Anita Chavez <chaveza@carlsbad.k12.nm.us>
2022-03-24 09:04:45 -0600

    save to local storage function

--- script.js
+++ script.js
@@ -2,12 +2,24 @@
 var today = moment();
 $("#currentDay").text(today.format("[Today is] dddd"));
 
+//Save 
+var textareaInput = document.querySelector(".description");
+var textareaOutput = document.querySelector(".description")
+var saveBtn = document.querySelector(".saveBtn");
 
+saveBtn.addEventListener("click", updateOutput);
+textareaInput.value = localStorage.getItem("content");
 
-
-
+function updateOutput() {
+	localStorage.setItem("content", textareaInput.value);
+    textareaOutput.textContent = textareaInput.value;
+}
 //color codes timeblocks to indicate past, present or future
 
 
-
+//if current hour is === textarea time then red
+//else if current hour is before textarea and not current time 
+//then grey 
+//else if current hour is after textarea time and not current time
+//then green
 
