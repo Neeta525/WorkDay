36cf0e6a256033aef303820e87fdcd874195f849
Anita Chavez <chaveza@carlsbad.k12.nm.us>
2022-03-24 16:24:22 -0600

    readme

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
-#  Work Day Calendar
+#  Work Day Calendar :calendar:
+
+
 
 ## Description
 
 This daily planner allows you to create a daily schedule. The current day is displayed at the top of the page. Each time block is color coded and is represented by pink for present time, grey for past and green for future events. The user can click inside a time block and enter their event, then click the blue save button so it will be saved in local storage.
+
+## Installation
+
+This application is available on most browsers.
+
+## Usage
+
+Screenshots :camera:
+ 
+
+## Built with
+- HTML
+- CSS
+- JAvascript
+
 
--- script.js
+++ script.js
@@ -3,23 +3,53 @@ var today = moment();
 $("#currentDay").text(today.format("[Today is] dddd"));
 
 //Save 
-var textareaInput = document.querySelector(".description");
-var textareaOutput = document.querySelector(".description")
-var saveBtn = document.querySelector(".saveBtn");
+// var textareaInput = document.querySelector(".description");
+// var textareaOutput = document.querySelector(".description")
+// var saveBtn = document.querySelector(".saveBtn");
 
-saveBtn.addEventListener("click", updateOutput);
+// saveBtn.addEventListener("click", updateOutput);
+// textareaInput.value = localStorage.getItem("content");
+
+// function updateOutput() {
+// 	localStorage.setItem("content", textareaInput.value);
+//     textareaOutput.textContent = textareaInput.value;
+// }
+
+
+var textareaInput = document.getElementById("description4");
+var textareaOutput = document.getElementById("description4")
+var saveBtn4 = document.getElementById("saveBtn4");
+
+saveBtn4.addEventListener("click", updateOutput);
 textareaInput.value = localStorage.getItem("content");
 
 function updateOutput() {
 	localStorage.setItem("content", textareaInput.value);
     textareaOutput.textContent = textareaInput.value;
 }
-//color codes timeblocks to indicate past, present or future
 
+var textareaInput = document.getElementById("description2");
+var textareaOutput = document.getElementById("description2")
+var saveBtn2 = document.getElementById("saveBtn2");
+
+saveBtn2.addEventListener("click", updateOutput);
+textareaInput.value = localStorage.getItem("content");
+
+function updateOutput() {
+	localStorage.setItem("content", textareaInput.value);
+    textareaOutput.textContent = textareaInput.value;
+}
+
+//color codes timeblocks to indicate past, present or future
 
 //if current hour is === textarea time then red
 //else if current hour is before textarea and not current time 
 //then grey 
 //else if current hour is after textarea time and not current time
 //then green
+
+
+var past;
+var present;
+var future;
 
