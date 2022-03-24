0b62c4524e795b1db892c86aaa6e91273a0e3349
Anita Chavez <anita.chavez@carlsbadschools.net>
2022-03-23 21:33:12 -0600

    layout corrections

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-
-
-
-
-
+// Displays current day at the top of the calendar
 var today = moment();
 $("#currentDay").text(today.format("[Today is] dddd"));
+
+
+
+
+
+//color codes timeblocks to indicate past, present or future
+
+
+
+
